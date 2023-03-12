20825287ac5df844866f9d025027989f4d67c61a
toro-test <bp2djbeqs7z@gmail.com>
2023-03-12 13:09:24 +0900

    web_dev comp

---
+++ src/php_app/web_dev/web/index.php
@@ -0,0 +1,38 @@
+<?php
+require("../app/functions.php");
+
+createToken();
+
+define('FILENAME','../app/messages.txt');
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+  validateToken();
+  $message = trim(filter_input(INPUT_POST,'message'));
+  $message = $message !== '' ? $message : '...';
+  $fp = fopen(FILENAME, 'a');
+  fwrite($fp,$message . "\n");
+  fclose($fp);
+  header('Location: http://localhost:9000/web_dev/web/result.php');
+  exit;
+}
+
+$messages = file(FILENAME,FILE_IGNORE_NEW_LINES);
+
+include("../_parts/_header.php");
+?>
+<ul>
+  <?php foreach($messages as $message) : ?>
+    <li><?= h($message) ?></li>
+    <?php endforeach; ?>
+</ul>
+
+<form action="" method="post">
+  <input type="text" name="message">
+  <button>post</button>
+  <input type="hidden" name="token" value="<?= h($_SESSION['token']) ?>">
+</form>
+
+
+<?php
+include("../_parts/_footer.php");
+?>
---
+++ src/php_app/web_dev/web/result.php
@@ -0,0 +1,16 @@
+<?php
+require("../app/functions.php");
+
+
+
+include("../_parts/_header.php");
+?>
+
+<p>message added!</p>
+
+<p><a href="index.php">Go back</a></p>
+
+
+<?php
+include("../_parts/_footer.php");
+?>
